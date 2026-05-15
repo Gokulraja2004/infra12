9bbd1b759627cdbbc30186e60e8d83680605b030
Gokulraja2004 <gg7537007@gmail.com>
2026-05-15 19:40:30 +0530

    hxhjs

---
+++ infra12-dev/modules/s3/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket1" {
+    type = string
+    description = "s3-bucket"
+  
+}
+
+variable "env" {
+    type = string
+    description = "dev"
+  
+}
+variable "name1" {
+    type = string
+    description = "name for s3"
+  
+}
